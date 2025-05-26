11dc16568a991a9db8ee6a0ac9295693bb9768c0
sandro Pereira <smpsandro1239@gmail.com>
2025-05-26 09:32:18 +0100

    Updated User model and auth config for JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,44 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    protected $fillable = ['name', 'email', 'password'];
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+        'phone',
+        'course_completion_year',
+        'photo',
+        'cv',
+        'company_name',
+        'company_city',
+        'company_website',
+        'company_description',
+        'company_logo',
+        'registration_status',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 
     public function getJWTIdentifier()
     {
