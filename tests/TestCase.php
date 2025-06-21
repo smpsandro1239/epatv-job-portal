c578c53d48c75e7d448f827c19d1a7e6eceabc84
Sandro Pereira Dev <smpsandro1239@gmail.com>
2025-06-21 14:23:00 +0100

    Fix CreatesApplication trait namespace in TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,4 +8,18 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use Illuminate\Foundation\Testing\CreatesApplication, DatabaseMigrations;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Ensure SQLite database is fresh
+        if (config('database.default') === 'sqlite') {
+            $databasePath = config('database.connections.sqlite.database');
+            if (file_exists($databasePath)) {
+                unlink($databasePath);
+            }
+            touch($databasePath);
+        }
+    }
 }
